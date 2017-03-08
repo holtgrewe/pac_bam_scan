1976b3dff04ce2c0c1299b6f82e10e0d80055747
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2017-03-08 15:15:25 +0100

    Minimal main program

--- src/pac_bam_scan.cpp
+++ src/pac_bam_scan.cpp
@@ -1,4 +1,20 @@
-int main()
+#include <iostream>
+#include <map>
+
+#include <sam.h>
+
+int main(int argc, char ** argv)
 {
+    if (argc != 2) {
+        std::cerr << "Usage: pac_bam_scan FILE.bam\n";
+        return 1;
+    }
+
+    samFile * samfile = sam_open(argv[1], "r");
+    bam_hdr_t * hdr = sam_hdr_read(samfile);
+
+    bam_hdr_destroy(hdr);
+    sam_close(samfile);
+
     return 0;
 }
