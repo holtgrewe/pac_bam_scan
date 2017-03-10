c908d2a371c4adca548407b16b250d72d6aad903
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2017-03-10 12:11:06 +0100

    Modern GCC

--- .travis-scripts/build.sh
+++ .travis-scripts/build.sh
@@ -2,6 +2,8 @@
 
 set -exuo pipefail
 
+export CXX="g++-4.9" CC="gcc-4.9"
+
 pushd htslib
 sed -i.bak -e 's~^prefix.*~prefix=./inst~' Makefile
 make install
@@ -9,7 +11,7 @@ popd
 
 mkdir -p build && pushd build
 export HTSLIB_ROOT=${PWD}/../htslib/inst
-cmake -DCMAKE_BUILD_TYPE=Release ..
-make
+cmake -DCMAKE_CXX_COMPILER=$(which $CXX) -DCMAKE_C_COMPILER=$(which $CC) -DCMAKE_BUILD_TYPE=Release ..
+make VERBOSE=1
 
-{ ./src/pac_bam_scan || true; } | grep Usage
+{ ./src/pac_bam_scan 2>&1 || true; } | grep Usage
